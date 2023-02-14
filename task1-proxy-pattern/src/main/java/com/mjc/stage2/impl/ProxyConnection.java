a4ced46c1b0755cac568e62c0dbc1b0b72dcbfcc
abboskhan <abbosxon28012004@gmail.com>
2023-02-14 19:29:18 +0500

    proxyConnection

--- task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
+++ task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
@@ -1,15 +1,34 @@
 package com.mjc.stage2.impl;
 
 
-public class ProxyConnection {
-    private RealConnection realConnection;
+import com.mjc.stage2.Connection;
 
+public class ProxyConnection implements Connection {
+    private RealConnection realConnection;
     public ProxyConnection(RealConnection realConnection) {
         this.realConnection = realConnection;
     }
 
     public void reallyClose() {
-        // Write your code here!
+        realConnection.close();
+    }
+
+    @Override
+    public void close() {
+        ConnectionPool.getInstance().releaseConnection(realConnection);
+    }
+
+    @Override
+    public boolean isClosed() {
+        return realConnection.isClosed();
     }
     // Implement methods here!
 }
+//    com.mjc.stage2.impl.ConnectionPool is a pool that contains all real connections created.
+//
+//        com.mjc.stage2.impl.RealConnection is a connection that implements the com.mjc.stage2.Connection interface and is used in connection pool.
+//
+//        Task: Implement proxy pattern for connection.
+//        Implement interface Connection in the ProxyConnection class (com.mjc.stage2.impl)
+//        The close() method should return connection to the connection pool
+//        Write a reallyClose() method in the ProxyConnection class that closes the real connection.
